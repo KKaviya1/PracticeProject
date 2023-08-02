b188175618c022c21b2d4509943a323f41e685cc
KKaviya1 <139097458+KKaviya1@users.noreply.github.com>
2023-08-02 10:45:26 +0530

    Add files via upload

---
+++ Section 10/Student/Student/DataStorage.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Student
+{
+    public class DataStorage
+    {
+        private static DataStorage _instance;
+        private List<Student> _students;
+        private List<Teacher> _teachers;
+        private List<Subject> _subjects;
+
+        private DataStorage()
+        {
+            _students = new List<Student>();
+            _teachers = new List<Teacher>();
+            _subjects = new List<Subject>();
+        }
+
+        public static DataStorage Instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    _instance = new DataStorage();
+                }
+                return _instance;
+            }
+        }
+
+        public void AddStudent(Student student)
+        {
+            _students.Add(student);
+        }
+
+        public void AddTeacher(Teacher teacher)
+        {
+            _teachers.Add(teacher);
+        }
+
+        public void AddSubject(Subject subject)
+        {
+            _subjects.Add(subject);
+        }
+
+        public List<Student> GetStudentsByClassAndSection(string classAndSection)
+        {
+            return _students.Where(s => s.ClassAndSection == classAndSection).ToList();
+        }
+
+        public List<Subject> GetSubjectsTaughtByTeacher(string teacherName)
+        {
+            return _subjects.Where(s => s.Teacher != null && s.Teacher.Name == teacherName).ToList();
+        }
+    }
+
+}
